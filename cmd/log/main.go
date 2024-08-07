64ff267b538ef9259c2830e528b156a9cb2225e4
Audun Stien <audun97@gmail.com>
2024-08-07 06:37:25 +0000

    main: rename variable for current time

--- cmd/log/main.go
+++ cmd/log/main.go
@@ -48,7 +48,7 @@ func main() {
 		os.Exit(0)
 	}
 
-	time := time.Now()
+	now := time.Now()
 
 	dirPath := filepath.Join(homeDir, strconv.Itoa(time.Year()), fmt.Sprintf("%02d", time.Month()))
 
@@ -57,7 +57,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	filePath := filepath.Join(dirPath, fmt.Sprintf("%02d.md", time.Day()))
+	filePath := filepath.Join(dirPath, fmt.Sprintf("%02d.md", now.Day()))
 	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_APPEND, 0755)
 	if err != nil {
 		if !errors.Is(err, fs.ErrNotExist) {
@@ -71,7 +71,7 @@ func main() {
 			os.Exit(1)
 		}
 
-		_, err = file.Write([]byte(fmt.Sprintf("# %s %02d/%02d/%d\n", time.Weekday().String(), time.Day(), int(time.Month()), time.Year())))
+		_, err = file.Write([]byte(fmt.Sprintf("# %s %02d/%02d/%d\n", now.Weekday().String(), now.Day(), int(now.Month()), now.Year())))
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "failed to write header to file %q: %s\n", filePath, err.Error())
 			file.Close()
@@ -80,7 +80,7 @@ func main() {
 	}
 
 	if !isViewOnly {
-		if _, err := file.Write([]byte(fmt.Sprintf("\n## %02d:%02d\n\n", time.Hour(), time.Minute()))); err != nil {
+		if _, err := file.Write([]byte(fmt.Sprintf("\n## %02d:%02d\n\n", now.Hour(), now.Minute()))); err != nil {
 			fmt.Fprintf(os.Stderr, "failed to write subheader to file %q: %s\n", filePath, err.Error())
 			file.Close()
 			os.Exit(1)
